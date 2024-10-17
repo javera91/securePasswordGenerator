5d878ff60d309e9dbc2c6ca7fe9b5b9e73700a23
mateocampo1004 <146900595+mateocampo1004@users.noreply.github.com>
2024-10-17 18:44:51 -0500

    txt
    estructura generador de contraseñas

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,2 +1,33 @@
 // src/pages/Home.tsx
 import React, { useState } from 'react';
+import GenerationControls from '../components/generationControls';
+import GeneratedPassword from '../components/generatedPassword';
+
+const Home: React.FC = () => {
+    const [contrasena, setContrasena] = useState('');
+    
+
+    const generarContrasena = (longitud: number, incluirMayusculas: boolean, incluirNumeros: boolean, incluirSimbolos: boolean) => {
+        const caracteres = 'abcdefghijklmnopqrstuvwxyz' +
+            (incluirMayusculas ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' : '') +
+            (incluirNumeros ? '0123456789' : '') +
+            (incluirSimbolos ? '!@#$%^&*()_+' : '');
+
+        let nuevaContrasena = '';
+        for (let i = 0; i < longitud; i++) {
+            nuevaContrasena += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+        }
+        setContrasena(nuevaContrasena);
+    };
+
+    
+
+    return (
+        <div>
+            <h1>Generador de Contraseñas Seguras</h1>
+            <GenerationControls onGenerar={generarContrasena} />
+            <GeneratedPassword contrasena={contrasena}  />
+        </div>
+    );
+};
+export default Home;
